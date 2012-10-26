4eddbd154dcc297f6e3df9b66f66b443a2085b9e
chuzheng <chuzheng@gmail.com>
2012-10-26 14:21:52 +0800

    base counter 2000

--- db/counter.js
+++ db/counter.js
@@ -3,6 +3,13 @@ var counter_coll   = db.collection('counter')
 
 exports.saveTaskId = function(cb) {
     counter_coll.findAndModify({ task_id : {$gt: 0}}, {}, {$inc : {task_id : 1}}, { new : true, upsert : true }, function(err, result) {
+        if (!result) {
+            counter_coll.update({ task_id : {$exists : true}}, {$set : { task_id : 2000}}, {}, function(err, result) {
+                cb(err, 2000)
+            })
+
+            return
+        }
         cb(err, result.task_id)
     })
 }
