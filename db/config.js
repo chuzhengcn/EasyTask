fc10a6b9b490d268ef69ebdea4d4f780729c37d6
chuzheng <chuzheng@gmail.com>
2012-10-26 12:25:49 +0800

    change db config

--- db/config.js
+++ db/config.js
@@ -1,3 +1,4 @@
 var mongo   = require('mongoskin')
-var db      = mongo.db('chuzhengcn:123456@ds037977.mongolab.com:37977/heroku_app7939210?auto_reconnect')
+// var db      = mongo.db('chuzhengcn:123456@ds037977.mongolab.com:37977/heroku_app7939210?auto_reconnect')
+var db      = mongo.db('chuzhengcn:123456@192.168.10.102:27017/easytask?auto_reconnect')
 exports.db  = db
