7ff1a36f048879523b50ff94339b28fd560aa27c
chuzheng <chuzheng@gmail.com>
2012-10-29 14:22:56 +0800

    changge port

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.configure('development', function(){
 })
 
 app.configure('production', function(){
-    app.set('port', process.env.PORT || 80)
+    app.set('port', process.env.PORT || 5000)
 })
 //-----------------------route-----------------------------------
 
