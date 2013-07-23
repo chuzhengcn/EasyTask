0154ace927c6db900fc6c5b9fad6bb34abb39a47
chuzheng <chuzheng@gmail.com>
2013-07-23 10:59:34 +0800

    master

--- model/data.js
+++ model/data.js
@@ -9,7 +9,7 @@ exports.project = [{name : 'adminnycs.syyx.cn'},
                    {name : 'stat.syyx.com'},{name : 'stat2.syyx.com'}, {name : 'vip.syyx.com'},
                    {name : 'user.syyx.com'}, {name : 'safe.syyx.com'},{name : 'pay.syyx.com'},
                    {name : 'fwg.syyx.cn'}, {name : 'ad.syyx.cn'},{name : 'adminpay.syyx.cn'},
-                   {name : 'kfwg.syyx.cn'},
+                   {name : 'kfwg.syyx.cn'},{name : 'jbl.syyx.com'},{name : 'nycs.jbl.syyx.com'},
                    {name : 'z.syyx.com'}, {name : 'others'}, {name : 'unknown'}]
 
 exports.statusNames = ['需求提交','任务已分配','开发已完成','已提交Dev','已提交Test','已提交Master','已提交Release',
